ef4742520688e3ded9edb1ce14701ecddf38044b
venkata sai ram <veadala@asu.edu>
2020-01-29 17:02:56 -0700

    Added Roll a ball along with the build

---
+++ Roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offsetposition;
+
+    void Start ()
+    {
+        offsetposition = transform.position - player.transform.position;
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position + offsetposition;
+    }
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rbody;
+    private int count;
+
+    void Start ()
+    {
+        rbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rbody.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
